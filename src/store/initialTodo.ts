44ed5c8cbbe43f1086b6d902d0135c655e677a65
arjdesign <subedi.aarjun@gmail.com>
2020-10-22 15:16:42 -0500

    initial tods and types

---
+++ src/store/initialTodo.ts
@@ -0,0 +1,19 @@
+import {Todo} from '../type/types'
+
+export const initialTodo:Array<Todo> = [
+    {text: "Wake up",
+importance: "high",
+complete: true},
+
+{text: "Wake up",
+importance: "high",
+complete: true},
+
+
+{text: "Wake up",
+importance: "high",
+complete: true}
+
+
+
+]
---
+++ src/type/types.d.ts
@@ -0,0 +1,5 @@
+export interface Todo{
+    test: string,
+    importance: string,
+    complete:boolean
+}
