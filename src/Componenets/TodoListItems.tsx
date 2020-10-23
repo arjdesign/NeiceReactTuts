49f41d41487dc6b6c42fe56d136f407203e10a4c
arjdesign <subedi.aarjun@gmail.com>
2020-10-23 12:29:44 -0500

    updates

---
+++ src/Componenets/TodoListItems.tsx
@@ -0,0 +1,29 @@
+//https://codesandbox.io/s/lively-bird-i7ypi
+
+import React  from "react"
+import {Todo, ToggleComplete, DeleteItem} from "../type/types"
+import {initialTodo} from '../store/initialTodo'
+
+interface ToDoListItemProps {
+todo?: Todo;
+toggleComplete?:ToggleComplete;
+deleteItem?: DeleteItem
+
+}
+
+export const ToDoListItems: React.FC<ToDoListItemProps> = (props) =>{
+    //const [todos, setTodos] = React.useState(initialTodo)
+    return(
+       <li>
+           <label >
+               <input type="checkbox"
+               onChange = {() => props.toggleComplete}
+               checked ={props.todo?.complete}/>
+               {props.todo?.text}
+               <button onClick = { ()=> props.deleteItem}>Delete</button>
+
+           </label>
+
+       </li>
+    )
+}
--- src/type/types.d.ts
+++ src/type/types.d.ts
@@ -1,5 +1,12 @@
 export interface Todo{
-    test: string,
-    importance: string,
+    text: string,
+    importance?: string,
     complete:boolean
 }
+
+// Remember: interface are good at defining object and type for functions. 
+export type AddItem = (item:string) => void
+
+export type DeleteItem = () => void
+
+export type ToggleComplete = (selectedItem: Todo) => void
